0ad734bd060d4d96ad825c947831cddb0cce4d7e
jserodio <serodio.jose@protonmail.ch>
2016-09-21 16:11:14 +0200

    cambios en mi rama
    aqui se describe, la primera linea es para establecer un titulo del
    COMMIT. a partir de la tercera linea se pone la descripción del commit.

--- gestion-peliculas/src/Main.java
+++ gestion-peliculas/src/Main.java
@@ -3,7 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("como hacer un commit");
+		System.out.println("en mi rama privada");
 	}
 
 }
