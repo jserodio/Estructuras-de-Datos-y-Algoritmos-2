2544d70f597c3bc73269e582f2a1a75f5a3742d7
Perez <Perez@192.168.2.10>
2016-09-27 19:31:14 +0200

    retocado un poco el menu principal

--- gestion-peliculas/src/Main.java
+++ gestion-peliculas/src/Main.java
@@ -1,17 +1,34 @@
+import java.util.Scanner;
+
 public class Main {
+
+	private static Scanner	teclado	= null;
+	private static String	elegido;
 
 	public static void main(String[] args) {
 
-		System.out.println("Cargando datos del fichero: ");
+		Main.teclado = new Scanner(System.in);
+		Main.elegido = Main.teclado.next();
+		switch (Main.elegido) {
+		case "leer":
+			System.out.println("leyendo");
+			break;
+		case "cargar":
+			System.out.println("Cargando datos del fichero: ");
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				System.out.println("error en el retraso");
+				e.printStackTrace();
+			}
+			Stopwatch timer = new Stopwatch();
 
-		Stopwatch timer = new Stopwatch();
-		
-		System.out.println(timer.elapsedTime() + " seconds.");
-		
-		SinglePeliculas.getSingle().cargarDatos("FilmsActors20162017.txt");
-		
-		System.out.println(timer.elapsedTime() + " seconds.");
+			System.out.println(timer.elapsedTime() + " seconds.");
 
+			SinglePeliculas.getSingle().cargarDatos("FilmsActors20162017.txt");
+
+			System.out.println(timer.elapsedTime() + " seconds.");
+			break;
+		}
 	}
-
 }
