372dc8bffd6a7a628f998f06931f859265518892
jserodio <serodio.jose@protonmail.ch>
2016-10-12 00:06:49 +0200

    quitando un warning, imports que no se usaban

--- gestion-peliculas/src/packCodigo/SinglePeliculas.java
+++ gestion-peliculas/src/packCodigo/SinglePeliculas.java
@@ -1,9 +1,7 @@
 package packCodigo;
-import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.io.Writer;
