df572b18390c7777657251a817c3d67992d04161
jserodio <serodio.jose@protonmail.ch>
2016-10-23 20:50:52 +0200

    mejorados los comentarios de CircularLinkedList.java

--- gestion-peliculas/src/packCircularLinkedList/CircularLinkedList.java
+++ gestion-peliculas/src/packCircularLinkedList/CircularLinkedList.java
@@ -12,104 +12,138 @@ public class CircularLinkedList<T> implements ListADT<T> {
 
 	// Constructor
 	public CircularLinkedList() {
-	     last = null;
+		last = null;
 		descr = "";
 		count = 0;
 	}
 	
 	public void setDescr(String nom) {
-	  descr = nom;
+		descr = nom;
 	}
 
 	public String getDescr() {
-	  return descr;
+		return descr;
 	}
 
+	/**
+	 * Elimina el primer elemento de la lista
+	 * Precondición: la lista tiene al menos un elemento
+	 * COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+	 */
 	public T removeFirst() {
-	// Elimina el primer elemento de la lista
-        // Precondición: la lista tiene al menos un elemento
-		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-return null;
+	
+		return null;
 	}
 
+	/**
+	 * Elimina el último elemento de la lista.
+	 * Precondición: la lista tiene al menos un elemento.
+	 * Postcondición: elimina el último elemento de la lista.
+	 * 
+	 * COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+	 */
 	public T removeLast() {
-	// Elimina el último elemento de la lista
-        // Precondición: la lista tiene al menos un elemento
-			// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+	 
 		return null;
+	}
 
-		   }
-
-
+	/**
+	 * Elimina un elemento concreto de la lista
+	 * 
+	 * COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+	 */
 	public T remove(T elem) {
-	//Elimina un elemento concreto de la lista
-		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
+	
 		return null;
 
 	}
-
+	
+	/**
+	 * Da acceso al primer elemento de la lista
+	 */
 	public T first() {
-	//Da acceso al primer elemento de la lista
 	      if (isEmpty())
 	          return null;
 	      else return last.next.data;
 	}
 
+	/**
+	 * Da acceso al último elemento de la lista
+	 */
 	public T last() {
-	//Da acceso al último elemento de la lista
 	      if (isEmpty())
 	          return null;
 	      else return last.data;
 	}
 
+	/**
+	 * Determina si la lista contiene el elemento dado.
+	 * 
+	 * @param elemento
+	 * @return TRUE si lo encuentra. FALSE si no.
+	 */
 	public boolean contains(T elem) {
+	
 		return false;
-
-
-		   }
-
-	public T find(T elem) {
-	//Determina si la lista contiene un elemento concreto, y develve su referencia, null en caso de que no esté
-		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-		return null;
-
-
 	}
 
-	public boolean isEmpty() 
-	//Determina si la lista está vacía
-	{ return last == null;};
+	/**
+	 * Determina si la lista contiene un elemento concreto,
+	 * y devuelve su referencia.
+	 * 
+	 * @param elemento
+	 * @return referencia al elemento. NULL en caso de no encontrarlo.
+	 */
+	public T find(T elem) {
+		
+		return null;
+	}
 	
-	public int size() 
-	//Determina el número de elementos de la lista
-	{ return count;};
+	/**
+	 * Determina si la lista está vacía.
+	 */
+	public boolean isEmpty() {
+		return last == null;
+	};
 	
-	/** Return an iterator to the stack that iterates through the items . */ 
-	   public Iterator<T> iterator() { return new ListIterator(); } 
+	/**
+	 * Determina el número de elementos de la lista.
+	 */
+	public int size() {
+		return count;
+	};
+	
+	/**
+	 *  Return an iterator to the stack that iterates through the items.
+	 */ 
+	   public Iterator<T> iterator() {
+		   return new ListIterator();
+	   } 
 
-	   // an iterator, doesn't implement remove() since it's optional 
+	   /**
+	    * Clase privada ListIterator.
+	    * COMPLETAR EL CODIGO Y CALCULAR EL COSTE 
+	    */
 	   private class ListIterator implements Iterator<T> {
 
 		@Override
 		public boolean hasNext() {
-			// TODO Auto-generated method stub
+			// TODO
 			return false;
 		}
 
 		@Override
 		public T next() {
-			// TODO Auto-generated method stub
+			// TODO
 			return null;
 		} 
 
-		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-
-
-
-	   } // private class
+	   } // Clase privada.
 		
-		
-         public void visualizarNodos() {
+		/**
+		 * Imprime por pantalla cada nodo.
+		 */
+        public void visualizarNodos() {
 			System.out.println(this.toString());
 		}
 
