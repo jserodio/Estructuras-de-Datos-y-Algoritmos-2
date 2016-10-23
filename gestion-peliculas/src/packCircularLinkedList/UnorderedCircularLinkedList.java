4c004d16297ef7c1f013242190ee00b297055183
jserodio <serodio.jose@protonmail.ch>
2016-10-23 20:32:50 +0200

    addToFront, addToRear y el opcional addAfter hechos.

--- gestion-peliculas/src/packCircularLinkedList/UnorderedCircularLinkedList.java
+++ gestion-peliculas/src/packCircularLinkedList/UnorderedCircularLinkedList.java
@@ -2,21 +2,81 @@ package packCircularLinkedList;
 
 public class UnorderedCircularLinkedList<T> extends CircularLinkedList<T> implements UnorderedListADT<T> {
 	
+	/**
+	 * Añade un elemento al comienzo de la lista
+	 * 
+	 * @param elemento a añadir
+	 */
 	public void addToFront(T elem) {
-	// añade un elemento al comienzo
-		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-
+		// crear el primer nodo
+		Node<T> nodo = new Node<T>(elem);
+		
+		if (this.size() == 0) {
+			nodo.next = nodo;
+			this.last = nodo;
+		}
+		
+		nodo.next = this.last.next;
+		this.last.next = nodo;	
+		
+		this.count++;
 	}
 
+	/**
+	 * Añade un elemento al final de la lista
+	 * 
+	 * @param elemento a añadir
+	 */
 	public void addToRear(T elem) {
-	// añade un elemento al final 
-		// COMPLETAR EL CODIGO Y CALCULAR EL COSTE
-
+		// crear el primer nodo
+		Node<T> nodo = new Node<T>(elem);
+		
+		if (this.size() == 0) {
+			nodo.next = nodo;
+			this.last = nodo;
+		}
+		
+		nodo.next = this.last.next;
+		this.last.next = nodo;
+		this.last = nodo;
+		
+		this.count++;
 	}
 	
+	/**
+	 * Añade elem detrás de otro elemento concreto, target,
+	 * que ya se encuentra en la lista.
+	 * Opcional.
+	 * 
+	 * Precondiciones: La lista tiene al menos 1 elemento.
+	 * 				   Elemento target existe en la lista.
+	 * 
+	 * Postcondiciones: Queda el elemento insertado justo después.
+	 *  
+	 * @param elemento, target
+	 */
 	public void addAfter(T elem, T target) {
-	// Añade elem detrás de otro elemento concreto, target,  que ya se encuentra en la lista
-		// ¡COMPLETAR OPCIONAL!
+		Node<T> nodo = new Node<T>(elem);
+		Node<T> nodoT = new Node<T>(target);
+		
+		// recorrer la lista hasta que el actual sea
+		// nodo T, entonces realizar las operaciones.
+		Node<T> current;
+		current = this.last.next;
+		
+		while (!current.data.equals(nodoT.data)) {
+			current = current.next;
+		}
+		
+		if (current.data.equals(this.last.data)) {
+			this.last = nodo;
+		}
+		
+		// Al salir del bucle, current es el target
+		nodo.next = current.next;
+		current.next = nodo;
+		
+		this.count++;
 	}
 
 }
