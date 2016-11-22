2d1ccfe680be2811b380be3a3382377ccfa8e492
jserodio <serodio.jose@protonmail.ch>
2016-11-22 19:37:56 +0100

    Test unit estanConectadas

--- gestion-peliculas/src/packPruebas/TestGraphHash.java
+++ gestion-peliculas/src/packPruebas/TestGraphHash.java
@@ -146,7 +146,72 @@ public class TestGraphHash {
 
 	@Test
 	public void testEstanConectadas() {
-		fail("Not yet implemented");
+		
+		// Caso grafo vacio
+		SinglePeliculas.getSingle().cargarDatos("ficheros/casoVacio.txt");
+		GraphHash grafo = new GraphHash();
+		grafo.crearGrafo(SinglePeliculas.getSingle());
+		// Test
+		assertFalse(grafo.estanConectadas("p1", "a1"));
+		
+		// Caso grafo con 1 pelicula y sin actores
+		Pelicula p1 = new Pelicula("p1");
+		SinglePeliculas.getSingle().insertarPelicula(p1);
+		grafo.crearGrafo(SinglePeliculas.getSingle());
+		// Test
+		assertFalse(grafo.estanConectadas("p1", "a1"));
+		
+		// Caso grafo con una peli y un actor relacionados
+		Actor a1 = new Actor("a1");
+		p1.insertarActor(a1);
+		a1.insertarPelicula(p1);
+		SinglePeliculas.getSingle().vaciarLista();
+		SinglePeliculas.getSingle().insertarPelicula(p1);
+		SingleActores.getSingle().insertarActor(a1);
+		grafo.crearGrafo(SinglePeliculas.getSingle());
+		// Test
+		assertTrue(grafo.estanConectadas(p1.getNombre(), a1.getNombre()));
+		
+		// Caso grafo con un actor en dos peliculas
+		p1 = new Pelicula("p1");
+		Pelicula p2 = new Pelicula("p2");
+		p1.insertarActor(a1);
+		p2.insertarActor(a1);
+		a1.insertarPelicula(p1);
+		a1.insertarPelicula(p2);
+		SingleActores.getSingle().vaciarLista();
+		SinglePeliculas.getSingle().vaciarLista();
+		SinglePeliculas.getSingle().insertarPelicula(p1);
+		SinglePeliculas.getSingle().insertarPelicula(p2);
+		SingleActores.getSingle().insertarActor(a1);
+		grafo.crearGrafo(SinglePeliculas.getSingle());
+		// Test
+		assertTrue(grafo.estanConectadas(p1.getNombre(), p2.getNombre()));
+		
+		// Caso grafo con una pelicula(3) sin actores
+		a1 = new Actor("a1");
+		Actor a2 = new Actor("a2");
+		p1 = new Pelicula("p1");
+		p2 = new Pelicula("p2");
+		Pelicula p3 = new Pelicula("p3");
+		a1.insertarPelicula(p1);
+		p1.insertarActor(a1);
+		a2.insertarPelicula(p2);
+		p2.insertarActor(a2);
+		SingleActores.getSingle().vaciarLista();
+		SinglePeliculas.getSingle().vaciarLista();
+		SingleActores.getSingle().insertarActor(a1);
+		SingleActores.getSingle().insertarActor(a2);
+		SinglePeliculas.getSingle().insertarPelicula(p1);
+		SinglePeliculas.getSingle().insertarPelicula(p2);
+		SinglePeliculas.getSingle().insertarPelicula(p3);
+		grafo.crearGrafo(SinglePeliculas.getSingle());
+		// Test
+		assertFalse(grafo.estanConectadas(a1.getNombre(), p2.getNombre()));
+		// Test
+		assertTrue(grafo.estanConectadas(p2.getNombre(), a2.getNombre()));
+		// Test
+		assertTrue(grafo.estanConectadas(p3.getNombre(), a1.getNombre()));
 	}
 
 }
