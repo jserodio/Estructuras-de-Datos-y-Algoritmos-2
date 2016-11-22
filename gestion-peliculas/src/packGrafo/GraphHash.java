8d4d4c0d50868fcf687390bfae11e34ac91b88f1
jserodio <serodio.jose@protonmail.ch>
2016-11-22 23:32:18 +0100

    crear grafo

--- gestion-peliculas/src/packGrafo/GraphHash.java
+++ gestion-peliculas/src/packGrafo/GraphHash.java
@@ -2,7 +2,10 @@ package packGrafo;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.stream.Collectors;
 
+import packCodigo.Actor;
+import packCodigo.Pelicula;
 import packCodigo.SinglePeliculas;
 
 public class GraphHash {
@@ -16,6 +19,38 @@ public class GraphHash {
 	 */
 	public void crearGrafo (SinglePeliculas lPeliculas) {
 		
+		g = new HashMap<String, ArrayList<String>>();
+		
+		if (!lPeliculas.getLista().isEmpty()) {
+			// declarar fuera para mejorar la fluidez
+			ArrayList<String> nombresActores = null;
+			ArrayList<String> nombresPelis = null;
+			
+			for (Pelicula p : lPeliculas.getLista()) // Coste M (numero de pelis totales) añadido
+			{
+				
+				// Por cada pelicula
+				// Si pelicula no esta en el grafo
+				if (g.get(p.getNombre()) == null) {
+					// Coste N (numero de actores de cada peli añadido)
+					nombresActores = (ArrayList<String>) p.getListaActores().stream()
+							 .map(a -> a.getNombre())
+							   .collect(Collectors.toList()); // !! a ver si no peta ese casting...
+					g.put(p.getNombre(), nombresActores);
+				}
+				
+				for (Actor a : p.getListaActores()) { // Coste N
+					// Si actor no esta en el grafo
+					if (g.get(a.getNombre()) == null) {
+						// Coste P (numero de peliculas de cada actor)
+						nombresPelis = (ArrayList<String>) a.getListaPeliculas().stream()
+								 .map(peli -> peli.getNombre())
+								   .collect(Collectors.toList()); // !! a ver si no peta ese casting...
+						g.put(a.getNombre(), nombresPelis);
+					}
+				}	
+			}
+		}
 	}
 	
 	/**
